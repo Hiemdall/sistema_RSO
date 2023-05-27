829abcfbecafcc1abdd978a8ed49e3518f744a6d
Heimdall Rojas <heimdallr20@gmail.com>
2023-05-27 10:49:48 -0500

    Ventana emergente

--- web/historico/index.php
+++ web/historico/index.php
@@ -5,10 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historial del Equipo</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    
-
-</head>
+    <link rel="stylesheet" type="text/css" href="styles1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+ </head>
 <body>
     <h1>Historial del Equipo</h1>
     <form method="post">
@@ -31,7 +30,7 @@
         $fechaActual = date('d/m/Y'); 
     ?>
     <label for="fecha">Fecha:</label>
-    <input type="text" id="fecha" name="fecha" value="<?php echo $fechaActual; ?>">
+    <input type="text" id="fecha" name="fecha" value="<?php echo $fechaActual; ?>" readonly>
 
     <?php
         // Establecer la zona horaria
@@ -40,7 +39,7 @@
         $horaActual = date('H:i:s A'); 
     ?>
     <label for="hora">Hora:</label>
-    <input type="text" id="hora" name="hora" value="<?php echo $horaActual; ?>">
+    <input type="text" id="hora" name="hora" value="<?php echo $horaActual; ?>" readonly>
 
     <label for="tipo_mant">Tipo de Mantenimiento:</label>
     <select id="tipo_mant" name="tipo_mant">
@@ -51,14 +50,24 @@
       <option value="Remoto">Remoto</option>
     </select>
 
+    <!--
     <label for="observacion">Observación:</label>
-    <textarea id="observacion" rows="4" name="observacion" style="width: 762px;"></textarea>
+    <textarea id="observacion" rows="4" name="observacion" style="width: 762px;"></textarea>   -->
+
+    
+        <label for="text1">Descripción del soporte:</label>
+        <textarea id="text1" rows="4" name="observacion" style="width: 762px;"></textarea>   
+         
+      
+    
+ 
+
 
     <label for="recomendaciones">Recomendaciones:</label>
     <textarea id="recomendaciones" rows="4" name="recomendaciones" style="width: 762px;"></textarea>
 <!--
     <label for="nom_tec">Nombre del Técnico:</label>
-    <input type="text" id="nom_tec" name="nom_tec">-->
+    <input type="text" id="nom_tec" name="nom_tec"> -->
 
     <label for="nom_tec">Nombre del Técnico:</label>
     <select id="nom_tec" name="nom_tec">
@@ -73,10 +82,24 @@
 
 <!-- Botones-->
     <input type="submit" name="agregar" value="Guardar">
-  
-    
-    
+   
+     
   </form>
+  <button class="help-button" id="openPopup" name="observacion"  title="Ayuda"><i class="fas fa-question"></i></button>
+
+       <!-- Ventana Emergente -->
+       <div class="popup">
+      <h1>Ejemplos de descripciones para el informe</h1>
+    <p>Realizamos una limpieza exhaustiva de todos los componentes internos y externos del equipo, incluyendo ventiladores, disipadores de calor y filtros de aire. Se eliminó todo el polvo y los residuos acumulados, lo que mejorará el flujo de aire y reducirá el riesgo de sobrecalentamiento. Se reemplazó la pasta térmica en el procesador para garantizar una disipación adecuada del calor. La nueva pasta térmica ayudará a mantener la temperatura del procesador en niveles óptimos y evitará problemas de sobrecalentamiento en el futuro.
+       Verificamos todos los cables y conexiones para asegurarnos de que estuvieran correctamente enchufados y en buen estado de funcionamiento.</p>
+       <h1>--------------------------------------------------------------------------------------</h1>
+    <p>Limpieza de hardware: Se llevó a cabo una limpieza minuciosa de los componentes internos y externos del equipo, incluyendo ventiladores, disipadores de calor y componentes de almacenamiento. Esto eliminó el polvo y los residuos acumulados, mejorando el flujo de aire y evitando el sobrecalentamiento. Limpieza de software: Se eliminaron archivos temporales y basura acumulada, lo que liberó espacio en el disco y optimizó el sistema. Además, se desinstalaron programas innecesarios y se realizaron actualizaciones de sistema y controladores para garantizar un funcionamiento óptimo.</p>
+       <h1>--------------------------------------------------------------------------------------</h1>
+    <p>Cambio de disco duro: Reemplazamos su antiguo disco duro por uno de estado sólido (SSD). Esto ha mejorado significativamente lavelocidad de lectura y escritura de datos, lo que resultará en tiempos de carga más rápidos y una mayor capacidad de respuesta del sistema en general. Actualización de memoria RAM: Incrementamos la capacidad de la memoria RAM de su equipo, lo que permitirá un mejor manejo de múltiples tareas y una mayor velocidad de procesamiento. Con esta actualización, su equipo podrá ejecutar aplicaciones exigentes de manera más eficiente y sin problemas de rendimiento.</p>
+    <button id="cerrar">Cerrar</button>  
+    </div>
+
+
 <!-- Acciones de los botones -->
   <?php
   //Acciones de los submit
@@ -101,6 +124,32 @@ if (isset($_POST['agregar'])) {
 
 <!--Script para buscar en tiempo real la cedula, llamando el archivo buscar.php-->
 <script>
+
+// Ventana emergente descripción del soporte
+document.getElementById('openPopup').addEventListener('click', function() {
+ 
+ var popup = document.querySelector('.popup');
+ popup.style.display = 'block';
+
+ var popupTexts = popup.getElementsByTagName('p');
+ for (var i = 0; i < popupTexts.length; i++) {
+   popupTexts[i].addEventListener('click', function() {
+     document.getElementById('text1').value = this.innerText;
+     popup.style.display = 'none';
+   });
+ }
+
+document.getElementById('cerrar').addEventListener('click', function() {
+popup.style.display = 'none';
+});
+
+return false; // Evita que el formulario se envíe
+});
+
+
+
+
+
 //La infomación de la base de datos va a cada input del formulario
 document.getElementById("serial").addEventListener("input", function() {
   var input = this.value;
@@ -123,6 +172,8 @@ document.getElementById("serial").addEventListener("input", function() {
   xhr.send("input=" + input);
 });
 </script>
+
+
 
 </body>
 </html>
